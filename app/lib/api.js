5e4ed851d08890b4d36ffac9f48f9f5db0be7667
git_repo_user <evalgit@example.com>
2023-11-08 10:19:39 +0100

    Refactor `loadPages`, update `@craftercms/experience-builder` package version

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,12 +1,7 @@
-import {
-  getDescriptor,
-  parseDescriptor,
-  getNavTree,
-  urlTransform
-} from '@craftercms/content';
+import { getDescriptor, getNavTree, parseDescriptor, urlTransform } from '@craftercms/content';
 import { firstValueFrom, map, switchMap } from 'rxjs';
 
-export async function getModel(path = "/site/website/index.xml") {
+export async function getModel(path = '/site/website/index.xml') {
   return await firstValueFrom(
     getDescriptor(path, { flatten: true }).pipe(
       map((descriptor) => parseDescriptor(descriptor, { parseFieldValueTypes: true }))
@@ -17,9 +12,9 @@ export async function getModel(path = "/site/website/index.xml") {
 }
 
 export async function getModelByUrl(webUrl = '/') {
-  console.log("webUrl", webUrl);
+  console.log(`webUrl: "${webUrl}"`);
   return await firstValueFrom(
-    urlTransform('renderUrlToStoreUrl', webUrl).pipe(
+    urlTransform('renderUrlToStoreUrl', webUrl || '/').pipe(
       switchMap((path) => getDescriptor(path, { flatten: true }).pipe(
         map((descriptor) => parseDescriptor(descriptor, { parseFieldValueTypes: true }))
       ))
@@ -45,14 +40,14 @@ export async function getModelById(id) {
   } else {
     composedPath = id ?? '/';
   }
-  console.log("composedPath", composedPath);
+  console.log(`composedPath: "${composedPath}"`);
   const model = await getModelByUrl(composedPath);
   return { model };
 }
 
 export async function getNav() {
   return await firstValueFrom(
-    getNavTree("/site/website", 1).pipe(
+    getNavTree('/site/website', 3).pipe(
       // Flatten the nav so that home shows at the same level as the children.
       map((navigation) => [
         {
@@ -69,27 +64,12 @@ export async function getNav() {
 
 
 export async function loadPages() {
-  // Call an external API endpoint to get posts
-  const CMS_API = `${process.env.NEXT_PUBLIC_CRAFTERCMS_HOST_NAME}/api/1/services/page.json?path=/site/website&depth=3&crafterSite=${process.env.NEXT_PUBLIC_CRAFTERCMS_SITE_NAME}`;
-
-  console.log("CMS URL >>>>>>>", CMS_API);
-  const res = await fetch(CMS_API);
-  console.log("res status", res.status);
-  const data = await res.json();
-  console.log("data >>>>>>>", data);
-
-  const paths = data.childItems
-    ?.filter((item) => item?.dom?.page?.["content-type"]?.indexOf("/page/") !== -1)
-    ?.map((item) => {
-      const segments = item.url
-        ?.split('/')
-        ?.filter((segment) => segment !== "site" && segment !== "website");
-      console.log("segments >>>>>>>", segments);
-      return {
-        params: {
-          id: segments,
-        }
-      }
-    });
+  const navigation = await getNav();
+  const paths = [];
+  const traverse = (item) => {
+    paths.push({ params: { id: item.url.split('/').slice(1) } });
+    item.subItems?.forEach(traverse);
+  };
+  navigation.forEach(traverse);
   return paths;
 }
